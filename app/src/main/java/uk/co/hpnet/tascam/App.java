6db2d0d957864080efdfbec7ddb00a4b32a2a41f
Calvin <calvin@hpnet.co.uk>
2026-02-02 22:11:21 +0000

    fix: Use system property for debug logging
    Use \${sys:tascam.logLevel:-WARN} in log4j2.xml to read the log level
    from a system property. Set the property early in run() and call
    Configurator.reconfigure() to apply the change.
    
    This is cleaner than casting to Log4j internal classes.

--- app/src/main/java/uk/co/hpnet/tascam/App.java
+++ app/src/main/java/uk/co/hpnet/tascam/App.java
@@ -1,6 +1,6 @@
 package uk.co.hpnet.tascam;
 
-import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -9,6 +9,7 @@ import uk.co.hpnet.tascam.client.TascamTcpClient;
 import uk.co.hpnet.tascam.model.Preset;
 
 import java.io.Console;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
@@ -40,13 +41,6 @@ public class App implements Callable<Integer> {
 
         @Override
         public Integer call() {
-            if (parent.debug) {
-                // Force load TascamTcpClient class to initialize its logger, then set level
-                org.apache.logging.log4j.core.Logger tcpLogger = 
-                    (org.apache.logging.log4j.core.Logger) org.apache.logging.log4j.LogManager.getLogger(TascamTcpClient.class);
-                tcpLogger.setLevel(Level.DEBUG);
-            }
-
             String password = promptForPassword();
             
             try (TascamClient client = new TascamTcpClient()) {
@@ -107,6 +101,15 @@ public class App implements Callable<Integer> {
      * Use this for testing; main() calls this then System.exit().
      */
     static int run(String[] args) {
+        // Set log level BEFORE Log4j initializes (reads ${sys:tascam.logLevel} from log4j2.xml)
+        if (Arrays.asList(args).contains("-d") || Arrays.asList(args).contains("--debug")) {
+            System.setProperty("tascam.logLevel", "DEBUG");
+        } else {
+            System.setProperty("tascam.logLevel", "WARN");
+        }
+        // Force Log4j2 to re-read configuration with updated system property
+        Configurator.reconfigure();
+        
         return new CommandLine(new App())
                 .addSubcommand("list", new ListCommand())
                 .execute(args);
